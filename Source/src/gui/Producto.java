729c36552195f90ea5620e4ba57c2ddf0dcee03b
David Abad Vich <davidabad10@gmail.com>
2016-03-19 17:12:36 +0100

    Add all project files

---
+++ Source/src/gui/Producto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author David
+ * @brief Clase auxiliar para almacenar productos
+ */
+public class Producto {
+    String nombre;
+    int stock;
+    double precio;
+    int codigo;
+}
